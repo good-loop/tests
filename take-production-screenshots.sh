3591d1a8b5d87b878577c1b73f9199cc1fc86fd4
DA <daniel.appel.winterwell@gmail.com>
2018-03-19 16:37:38 +0000

    added a delay-before-running function

--- take-production-screenshots.sh
+++ take-production-screenshots.sh
@@ -2,6 +2,14 @@
 
 # Take test Screenshots
 
+#First argument is optional,  it is an int of how many seconds to wait before running this script.  Default value is 0.5
+if [[ $1 = '' ]]; then
+	sleep 0.5
+else
+	sleep $1
+fi
+
+
 SOURCE_JS=('compiled/good-loop-live-demo.js' 'compiled/as-player.js')
 
 for js_file in ${SOURCE_JS[*]}; do
--- take-test-screenshots.sh
+++ take-test-screenshots.sh
@@ -2,6 +2,13 @@
 
 # Take test Screenshots
 
+#First argument is optional,  it is an int of how many seconds to wait before running this script.  Default value is 0.5
+if [[ $1 = '' ]]; then
+	sleep 0.5
+else
+	sleep $1
+fi
+
 SOURCE_JS=('compiled/rectangle-brand-funded.js' 'compiled/rectangle-countdown.js' 'compiled/rectangle-default.js' 'compiled/testas-player.js')
 
 for js_file in ${SOURCE_JS[*]}; do
