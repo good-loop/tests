d92db7e64bf21a8ca40d7f065b17486009be654f
DA <daniel.appel.winterwell@gmail.com>
2018-03-19 15:18:24 +0000

    renaming files, organising in to dirs

--- src/source-rectangle-default.js
+++ src/source-rectangle-default.js
@@ -4,8 +4,11 @@ const puppeteer = require('puppeteer');
   const browser = await puppeteer.launch();
   const page = await browser.newPage();
   const loaded = page.waitForNavigation({waitUntil: 'networkidle0'});
+  const mouse = page.mouse;
   await page.setViewport({width: 1920,height: 1080});
-  await page.goto('http://rectangle.good-loop.com');
+  await page.goto('http://rectangle.good-loop.com/?gl.variant=default');
+  await loaded;
+  await mouse.click(175,202);
   await loaded;
   await page.screenshot({path: 'rectangle.png'});
 
