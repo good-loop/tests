405815240bc7d00d606d8287695766a5ca6004e7
DA <daniel.appel.winterwell@gmail.com>
2018-03-19 16:25:52 +0000

    added another dir to ignore, and finished the compile.sh script

--- compile.sh
+++ compile.sh
@@ -2,10 +2,11 @@
 
 #compile/transpile/translate the source JS' for screenshots (from es2016 to es2015)
 
-ALL_SOURCES=$(find src/ -iname "*.js")
+ALL_SOURCES=$(cd src/ && find -iname "*.js")
 
-for source in ${ALL_SOURCES[*]}; do
+for js_file in ${ALL_SOURCES[*]}; do
 	printf "\n"
-	printf "\nBabeling $source ..."
-	babel $source --out-file compiled/$source
+	printf "\nBabeling $js_file ..."
+	babel src/$js_file --out-file compiled/$js_file
+	printf "\n"
 done
